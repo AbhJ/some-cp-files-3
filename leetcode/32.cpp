9e9a30a94d7d83355a6322515edda52c8b3e0886
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2022-01-11 11:15:00 +0530

    Updates

--- leetcode/32.cpp
+++ leetcode/32.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-	int ans;
-
-	void cnt(string s, int z) {
-		int n = s.length(), cnt = 0, o = 0, e = 0;
-		for(int i = 0; i < s.length(); i++) {
-			if(s[i] == '(')
-				cnt++, o++;
-			if(s[i] == ')')
-				cnt--, e++;
-			if(cnt < 0 and z == 0)
-				cnt = o = e = 0;
-			if(cnt > 0 and z == 1)
-				cnt = o = e = 0;
-			if(cnt == 0)
-				ans = max(ans, o << 1);
+	int longestValidParentheses (string s) {
+		int l = s.length();
+		stack <int> S;
+		vector<int> lengths;
+		for (int i = 0; i < l; i++) {
+			if (s[i] == '(')
+				S.push (i);
+			else {
+				if (S.empty() == 0 and s[S.top()] == '(')
+					S.pop();
+				else
+					S.push (i);
+				// WE ONLY HAVE UNSOLVED PARANTHESIS IN STACK
+			}
 		}
-	}
-
-	int longestValidParentheses(string s) {
-		ans = 0;
-		cnt(s, 0);
-		reverse(s.begin(), s.end());
-		cnt(s, 1);
-		return ans;
+		int fin = l;
+		while (S.empty() == 0) {
+			int sta = S.top();
+			S.pop();
+			lengths.emplace_back (fin - sta - 1);
+			fin = sta;
+		}
+		int sta = - 1;
+		lengths.emplace_back (fin - sta - 1);
+		return *max_element (lengths.begin(), lengths.end());
 	}
 };
-
