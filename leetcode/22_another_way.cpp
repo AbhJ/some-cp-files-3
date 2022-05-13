d303deb6a29048a1a15868649e6e4d2cd10cbd26
abhj <mitraabhijay@gmail.com>
2022-05-13 23:13:36 +0530

    Updates

---
+++ leetcode/22_another_way.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+	vector<string>v;
+	void f (string &s, int open, int &n) {
+		int len = s.length();
+		int close = len - open;
+		if (close > open)
+			return;
+		if (len == 2 * n) {
+			if (open == n)
+				v.emplace_back (s);
+			return;
+		}
+		s += "(";
+		f (s, open + 1, n);
+		s.pop_back();
+		if (open > close) {
+			s += ")";
+			f (s, open, n);
+			s.pop_back();
+		}
+	}
+
+	vector<string> generateParenthesis (int n) {
+		v.clear();
+		string s;
+		f (s, 0, n);
+		return v;
+	}
+};
