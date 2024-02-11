5cb6087e28f5206a821dc996b97f47fd9e294ca9
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2024-02-11 17:14:46 +0530

    updated 1928 contest files

---
+++ 1928B.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <set>
+#include <climits>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <cctype>
+#include <bitset>
+#define int          long long int
+#define mp           make_pair
+#define pb           emplace_back
+#define F            first
+#define S            second
+using vi       =     std::vector<int>;
+using vvi      =     std::vector<vi>;
+using pii      =     std::pair<int, int>;
+using vpii     =     std::vector<pii>;
+using vvpii    =     std::vector<vpii>;
+using namespace std;
+const int inf  =     1e18 + 10;
+const int N    =     2e6 + 10;
+int n;
+
+void solve() {
+	cin >> n;
+	set <int> s;
+	vi a;
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		s.emplace (x);
+	}
+	for (auto i : s) {
+		a.pb (i);
+	}
+	int ans = 1;
+	for (int i = 0; i < a.size(); i++) {
+		int cnt = lower_bound (a.begin() + i, a.end(),
+		                       a[i] + n) - (a.begin() + i);
+		ans = max (ans, min (cnt, (int) a.size()));
+	}
+	cout << ans;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio (0);
+	cin.tie (0);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << "\n";
+	}
+	return 0;
+}
