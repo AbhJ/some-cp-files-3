274b5863a3769a1143887ba921180ac985f3e08a
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2024-02-19 23:25:56 +0530

    updated practice problems

---
+++ 1929C.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <set>
+#include <climits>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <cctype>
+#include <bitset>
+#define int          long long int
+#define mp           make_pair
+#define pb           emplace_back
+#define F            first
+#define S            second
+using vi       =     std::vector<int>;
+using vvi      =     std::vector<vi>;
+using pii      =     std::pair<int, int>;
+using vpii     =     std::vector<pii>;
+using vvpii    =     std::vector<vpii>;
+using namespace std;
+const int inf  =     1e18 + 10;
+const int N    =     2e6 + 10;
+int k, x, a;
+
+void solve() {
+	cin >> k >> x >> a;
+	if (a - x < 0)
+		cout << "NO";
+	else {
+		int spent = 0;
+		for (int i = 0; i <= min (x, (int) 1e6); i++) {
+			spent += spent / (k - 1) + 1;
+			if (spent > a) {
+				cout << "NO";
+				return;
+			}
+		}
+		cout << "YES";
+	}
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio (0);
+	cin.tie (0);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << "\n";
+	}
+	return 0;
+}
