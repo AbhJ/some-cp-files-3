5a5cc1c78c63e43532c09b5be70d97504bf07ffb
Abhijay Mitra <abhijaymitra@ABJ-MacBook-Pro.local>
2024-03-11 00:22:44 +0530

    updated 1935D.cpp

---
+++ 1935D.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <set>
+#include <climits>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <cctype>
+#include <bitset>
+#define int          long long int
+#define mp           make_pair
+#define pb           emplace_back
+#define F            first
+#define S            second
+using vi       =     std::vector<int>;
+using vvi      =     std::vector<vi>;
+using pii      =     std::pair<int, int>;
+using vpii     =     std::vector<pii>;
+using vvpii    =     std::vector<vpii>;
+using namespace std;
+const int inf  =     1e18 + 10;
+const int N    =     2e6 + 10;
+int n, c, a[N], ans, odd, eve;
+
+void solve() {
+	cin >> n >> c;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		if (a[i] & 1)
+			ans += ++odd;
+		else
+			ans += ++eve;
+		ans -= 1 + (a[i] >> 1);
+		ans -= c - a[i] + 1;
+	}
+	cout << ans + ((c + 1) * (c + 2) >> 1);
+	ans = odd = eve = 0;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio (0);
+	cin.tie (0);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+		cout << "\n";
+	}
+	return 0;
+}
