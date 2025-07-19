a7dce467c610dfcefbd076a1db6a910dec437eb9
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2025-07-19 12:43:14 +0530

    added 406c

---
+++ atcoder/abc406_c.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <set>
+#include <climits>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <cctype>
+#include <bitset>
+#define int          long long int
+#define mp           make_pair
+#define pb           emplace_back
+#define F            first
+#define S            second
+using vi       =     std::vector<int>;
+using vvi      =     std::vector<vi>;
+using pii      =     std::pair<int, int>;
+using vpii     =     std::vector<pii>;
+using vvpii    =     std::vector<vpii>;
+using namespace std;
+const int inf  =     1e18 + 10;
+const int N    =     2e6 + 10;
+int n, a[N], ans, disSincelastInc, inc;
+
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	for (int i = 1; i < n; i++) {
+		if (a[i - 1] < a[i]) {
+			inc++;
+			ans += disSincelastInc;
+		}
+		else if (inc) {
+			disSincelastInc = inc;
+			inc = 0;
+		}
+		// cout << i << " inc: " << inc << " disSincelastInc: " << disSincelastInc <<
+		//      " ans: " << ans << "\n";
+	}
+	cout << ans;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio (0);
+	cin.tie (0);
+	solve();
+	return 0;
+}
