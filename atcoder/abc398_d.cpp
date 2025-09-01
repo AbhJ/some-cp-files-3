2501802569b1ac4c5473e304422ee653aed0dab9
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2025-09-01 16:27:46 +0530

    added files

---
+++ atcoder/abc398_d.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <numeric>
+#include <vector>
+#include <iterator>
+#include <map>
+#include <set>
+#include <climits>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <cctype>
+#include <bitset>
+#define int          long long int
+#define mp           make_pair
+#define pb           emplace_back
+#define F            first
+#define S            second
+using vi       =     std::vector<int>;
+using vvi      =     std::vector<vi>;
+using pii      =     std::pair<int, int>;
+using vpii     =     std::vector<pii>;
+using vvpii    =     std::vector<vpii>;
+using namespace std;
+const int inf  =     1e18 + 10;
+const int N    =     2e6 + 10;
+int n, r, c, fx, fy;
+string s;
+set<pii> vis;
+
+void solve() {
+	cin >> n >> r >> c >> s;
+	vis.emplace (0, 0);
+	for (int i = 0; i < n; i++) {
+		if (s[i] == 'N') {
+			fx++;
+			r++;
+		}
+		if (s[i] == 'S') {
+			fx--;
+			r--;
+		}
+		if (s[i] == 'W') {
+			fy++;
+			c++;
+		}
+		if (s[i] == 'E') {
+			fy--;
+			c--;
+		}
+		vis.emplace (fx, fy);
+		cout << vis.count (mp (r, c));
+	}
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio (0);
+	cin.tie (0);
+	solve();
+	return 0;
+}
