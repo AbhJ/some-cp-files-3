e106dca0ebf77a2aa4928438226a47bcce7da366
Abhijay Mitra <48042655+AbhJ@users.noreply.github.com>
2026-06-22 01:49:57 +0530

    added

---
+++ leetcode/1035.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& a, vector<int>& b) {
+        int n = a.size(), m = b.size();
+        vector <vector<int>> dp(n + 1, vector <int> (m + 1));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a[i - 1] == b[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max (dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[n][m];
+    }
+};
